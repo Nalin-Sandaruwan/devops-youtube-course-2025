2c029840b5d5265f651fb386f30e106f0e4ef705
Nalin-Sandaruwan <creationbynalin@gmail.com>
2026-01-17 01:23:49 +0530

    fix(api): update API base URL and refine CORS settings

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
     'http://localhost:5173', // Vite dev server / mapped frontend
-    'http://3.26.242.54:5173', // Frontend when accessed via server public IP
+    'http://3.26.242.54:5173',
+    '' // Frontend when accessed via server public IP
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
